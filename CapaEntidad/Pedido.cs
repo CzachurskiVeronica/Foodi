e5d3e5c75f8e2920e4cae89a4a0d3ff7a20de597
Agustin-Cab <agustincab1915@gmail.com>
2025-04-27 13:08:55 -0300

    creacion de carpetas

---
+++ CapaEntidad/Pedido.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CapaEntidad
+{
+    public class Pedido
+    {
+        public int Id_Pedido { get; set; }
+        public float Total { get; set; }
+        public string Fecha { get; set; }
+        public Pago Id_Pago { get; set; }
+
+        public Usuario Id_Usuario { get; set; }
+        public string Nro_factura { get; set; }
+        public Mesa Id_Mesa { get; set; }
+        public List<PedidoDetalle> PedidoDetalle { get; set; }
+
+    }
+}
