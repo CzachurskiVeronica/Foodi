597049fc69908e57e7a4f9763452adb876ff0377
Agustin-Cab <agustincab1915@gmail.com>
2025-04-27 13:04:48 -0300

    Creacion de las entidades Rol, Usuario y Mesa

---
+++ CapaEntidad/Mesa.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CapaEntidad
+{
+    public class Mesa
+    {
+        public int Id_Mesa {get;set;}
+        public int Cap_Personas_Max { get;set;}
+        public int Ocupado { get;set;}
+
+
+    }
+}
---
+++ CapaEntidad/Usuario.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CapaEntidad
+{
+    public class Usuario
+        {
+            public int Id_Usuario { get; set; }
+            public int DNI { get; set; }
+            public string Nombre { get; set; }
+            public string Apellido { get; set; }
+            public string Fecha_Incorporación { get; set; }
+            public string Contraseña { get; set; }
+            public int Activo { get; set; }
+            public Rol Id_rol { get; set; }
+
+    }
+   
+}
