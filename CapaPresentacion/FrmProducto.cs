54d16a25ec777716dea11dae986bbf48589cc7dc
Pietro <pietrocalathaki@gmail.com>
2025-04-28 22:25:14 -0300

    frm menu

--- CapaPresentacion/FrmProducto.cs
+++ CapaPresentacion/FrmProducto.cs
@@ -16,5 +16,10 @@ namespace CapaPresentacion
         {
             InitializeComponent();
         }
+
+        private void FrmProducto_Load(object sender, EventArgs e)
+        {
+
+        }
     }
 }
