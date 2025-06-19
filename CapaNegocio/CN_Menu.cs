829ef9dd1a2a7ac17d2ce09a1f31f479a2bb0bab
Veronica Czachurski <czachurski.veronica@gmail.com>
2025-06-19 18:03:27 -0300

    referencias agregadas

--- CapaNegocio/CN_Menu.cs
+++ CapaNegocio/CN_Menu.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-﻿using CapaDatos;
+using CapaDatos;
 using CapaEntidad;
 
 namespace CapaNegocio
@@ -13,21 +13,25 @@ namespace CapaNegocio
         // Retorna la lista de la Capa de Datos menu
         private CD_Menu obj_menu = new CD_Menu();
 
+        // Retorna la lista de la Capa de Datos menu
         public List<Menu> Listar()
         {
             return obj_menu.Listar();
         }
 
+        // Registra un nuevo menu
         public int Registrar(Menu menu, out string Mensaje)
         {
             return obj_menu.RegistrarMenu(menu, out Mensaje);
         }
 
+        // Edita un menu existente
         public bool Editar(Menu menu, out string Mensaje)
         {
             return obj_menu.EditarMenu(menu, out Mensaje);
         }
 
+        // Elimina un menu
         public bool Eliminar(Menu menu, out string Mensaje)
         {
             return obj_menu.EliminarMenu(menu, out Mensaje);
